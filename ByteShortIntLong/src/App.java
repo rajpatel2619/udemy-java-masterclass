f8191d33cbff1e886149a62b5846b74047549a47
raj patel <kakhilesh79@gmail.com>
2022-05-13 00:19:27 +0530

    Day 1  -  29/401 videos completed

--- ByteShortIntLong/src/App.java
+++ ByteShortIntLong/src/App.java
@@ -2,5 +2,38 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("int max val " + Integer.MAX_VALUE);
+        // wrapper classes for all 8 primitve data types..
+        // eg: for int - INTEGER.MIN_VALUE
+
+        //underflow and overflow - (min - 1),(max+1)  ...
+        
+        // interesting underscore...
+        int num = 2_1_3;
+        // System.out.println(num);
+
+
+        // 8 primitive data types in java
+        // byte , boolean , int, char, short, long, double, float......
+
+        // byte - 8bit
+        //short - 16bit
+        // int  - 32bit
+
+        // System.out.println(100L);
+
+        // ..................................
+        // casting in java
+        // int total = (int)(2365765643/3);
+
+        System.out.println(3444 +
+        434);
+
+        // floating point
+        // float - single precesion - 32 bits
+        // double - double precision - 64 bits
+        
+
+
+
     }
 }
